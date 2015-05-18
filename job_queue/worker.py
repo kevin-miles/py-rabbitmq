4dba2baa6a112ebc06b85a90a2f19b07a137c1ed
Kevin Miles <kevin.miles@dws.la>
2015-05-17 21:21:00 -0700

    add message acknowledgement
    if a worker dies messages will now be redelivered

--- job_queue/worker.py
+++ job_queue/worker.py
@@ -14,9 +14,9 @@ def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
     time.sleep( body.count('.') )
     print " [x] Done"
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel.basic_consume(callback,
-                      queue='job',
-                      no_ack=True)
+                      queue='job')
 
 channel.start_consuming()
